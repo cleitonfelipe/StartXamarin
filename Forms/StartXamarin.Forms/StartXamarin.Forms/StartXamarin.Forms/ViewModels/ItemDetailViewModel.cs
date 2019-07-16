25274fe776ec1b41f260fca2185f687a4d6ec541
cnds <cleiton.de-moraes@gft.com>
2019-07-16 11:50:40 -0300

    Add Xamarin.Forms Project

---
+++ Forms/StartXamarin.Forms/StartXamarin.Forms/StartXamarin.Forms/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using StartXamarin.Forms.Models;
+
+namespace StartXamarin.Forms.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
